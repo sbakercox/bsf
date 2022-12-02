fbb69654da989e2e8b3bb904152b81776e05f8a3
Shaquille Baker <sbakercox@gmail.com>
2022-12-01 22:55:28 -0500

    addition of workout & program models

--- models/Profile.js
+++ models/Profile.js
@@ -1,23 +1,22 @@
 const mongoose = require('mongoose')
-const bcrypt = require("bcrypt");
 
 const ProfileSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    alias: { type: String, required: true },
-    role: { type: String, required: true },
-    profileImg: { type: String},
-    profileImgId: { type: String},
-    initImg: { type: String},
-    initImgId: { type: String},
-    fitnessGoal: { type: String, required: true },
-    currentWeight: { type: String, required: true },
-    weightGoal: { type: String, required: true },
-    currentDiet: { type: String, required: true },
-    dietaryGoal: { type: String, required: true },
-    activityLevel: { type: String, required: true },
-    daysAvailable: { type: Array, required: true },
-    timesAvailable: { type: Array, required: true },
-    medConcerns: { type: String, required: true },
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    alias: {type: String, required: true},
+    role: {type: String, required: true},
+    profileImg: {type: String},
+    profileImgId: {type: String},
+    initImg: {type: String},
+    initImgId: {type: String},
+    fitnessGoal: {type: String, required: true},
+    currentWeight: {type: String, required: true},
+    weightGoal: {type: String, required: true},
+    currentDiet: {type: String, required: true},
+    dietaryGoal: {type: String, required: true},
+    activityLevel: {type: String, required: true},
+    daysAvailable: {type: Array, required: true},
+    timesAvailable: {type: Array, required: true},
+    medConcerns: {type: String, required: true},
   });
 
   module.exports = mongoose.model('Profile', ProfileSchema)
--- models/User.js
+++ models/User.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose')
-const bcrypt = require("bcrypt");
 
 const GoogleSchema = new mongoose.Schema({
-    googleId: { type: String, required: true },
-    displayName: { type: String, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-    profile: { type: Boolean, default: false },
+    googleId: {type: String, required: true },
+    displayName: {type: String, required: true },
+    firstName: {type: String, required: true },
+    lastName: {type: String, required: true },
+    createdAt: {type: Date, default: Date.now },
+    profile: {type: Boolean, default: false },
   });
 
   module.exports = mongoose.model('User', GoogleSchema)
